4f2bebbfa5d930e6bc79fcdf08cdb01ef21d2110
Neil Earlam <neil.earlam@clearskylogic.com>
2020-04-19 00:58:10 +0100

    Updated Extensions

--- SimpleJwtProvider/Extenstions/ServiceCollectionExtension.cs
+++ SimpleJwtProvider/Extenstions/ServiceCollectionExtension.cs
@@ -59,9 +59,9 @@ namespace SimpleJwtProvider.Extenstions
         /// </summary>
         /// <param name="services">IServiceCollection</param>
         /// <param name="options">Confiuration Options for the JWT Token</param>
-        /// <param name="responseBody">Custom Response Body to be sent on either Authentication failed or Access Forbidden</param>
+        /// <param name="authenticationFailedResponse">Custom Response Body to be sent on either Authentication failed or Access Forbidden</param>
         public static void AddJwtAuthentication(this IServiceCollection services,
-            JwtConfigurationOptions options, object responseBody)
+            JwtConfigurationOptions options, object authenticationFailedResponse)
         {
             services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
                 .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, builder =>
@@ -83,7 +83,7 @@ namespace SimpleJwtProvider.Extenstions
                     {
                         OnAuthenticationFailed = context =>
                         {
-                            var bodyData = ConvertModelForSending(responseBody);
+                            var bodyData = ConvertModelForSending(authenticationFailedResponse);
                             if (context.Exception is SecurityTokenExpiredException)
                             {
                                 context.Response.Headers.Add("RefreshToken", "true");
@@ -92,10 +92,56 @@ namespace SimpleJwtProvider.Extenstions
                             context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
                             context.Response.Body.WriteAsync(bodyData, 0, bodyData.Length);
                             return Task.CompletedTask;
+                        }
+                    };
+                });
+        }
+
+        /// <summary>
+        /// This method will set up the Authentication Scheme 
+        /// To be JWT and will set the OnAuthentication and OnForbidden
+        /// Events to be the responseBody object that is passed in
+        /// </summary>
+        /// <param name="services">IServiceCollection</param>
+        /// <param name="options">Confiuration Options for the JWT Token</param>
+        /// <param name="authenticationFailedResponse">Custom Response Body to be sent on Authentication failed</param>
+        /// <param name="forbiddenResponse">Custom Response Body to be sent on Forbidden Code</param>
+        public static void AddJwtAuthentication(this IServiceCollection services,
+            JwtConfigurationOptions options, object authenticationFailedResponse, object forbiddenResponse)
+        {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, builder =>
+                {
+                    builder.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ClockSkew = TimeSpan.Zero,
+                        ValidIssuer = options.Issuer,
+                        ValidAudience = options.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(options.SecretKey))
+                    };
+
+                    builder.Events = new JwtBearerEvents()
+                    {
+                        OnAuthenticationFailed = context =>
+                        {
+                            if (context.Exception is SecurityTokenExpiredException)
+                            {
+                                context.Response.Headers.Add("RefreshToken", "true");
+                            }
+                            var bodyData = ConvertModelForSending(authenticationFailedResponse);
+                            context.Response.ContentType = "application/json";
+                            context.Response.StatusCode = (int)HttpStatusCode.Unauthorized;
+                            context.Response.Body.WriteAsync(bodyData, 0, bodyData.Length);
+                            return Task.CompletedTask;
                         },
                         OnForbidden = context =>
                         {
-                            var bodyData = ConvertModelForSending(responseBody);
+                            var bodyData = ConvertModelForSending(forbiddenResponse);
                             context.Response.ContentType = "application/json";
                             context.Response.StatusCode = (int)HttpStatusCode.Forbidden;
                             context.Response.Body.WriteAsync(bodyData, 0, bodyData.Length);
